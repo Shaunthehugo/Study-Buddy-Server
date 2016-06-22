0cd222197aa3338c58e3b45c7de6a976b07678e4
Shaunthehugo <Shaun.dougherty@me.com>
2016-06-22 17:43:46 -0400

    Send to index

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.use(mountPath, api);
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
-  res.status(200).send('Oh shit, wuddup!');
+  res.status(200).send('/index.html');
 });
 
 // There will be a test page available on the /test path of your server url
