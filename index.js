7a124faa25e17ccdb6b5f1755f0a530c1c50f780
Shaunthehugo <Shaun.dougherty@me.com>
2016-06-22 18:31:41 -0400

    Fixed index.js

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.use(mountPath, api);
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
-  res.status(200).sendFile(path.join(__dirname, '/public/index.html'));
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 // There will be a test page available on the /test path of your server url
